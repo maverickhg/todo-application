2cca6e0a631c645f75b2d2684f2cd4d8456e2a4a
maverickhg <doncic1990@gmail.com>
2022-12-16 16:08:01 +0900

    text

--- src/main/java/com/codestates/todoapplication/toDoController.java
+++ src/main/java/com/codestates/todoapplication/toDoController.java
@@ -1,9 +1,11 @@
 package com.codestates.todoapplication;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class toDoController {
+	@GetMapping
 	public static void main(String[] args) {
 		System.out.println("To-do Application !");
 	}
