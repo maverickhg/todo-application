d20bd0610ccbdd37579a8441e0aed90a59fef94a
maverickhg <110888074+maverickhg@users.noreply.github.com>
2022-12-16 16:03:10 +0900

    Add files via upload

---
+++ src/main/java/com/codestates/todoapplication/toDoController.java
@@ -0,0 +1,10 @@
+package com.codestates.todoapplication;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class toDoController {
+	public static void main(String[] args) {
+		System.out.println("To-do Application !");
+	}
+}
